536a2c9cddefd5d7d2698a341bf4feabbda01827
Wesley Seago <wseago2@fullsail.edu>
2013-08-13 15:07:25 -0400

    Add practice4.

---
+++ PracticeWeek2/Practice4/Practice4/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Practice4
+//
+//  Created by Wesley Seago on 8/13/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
