20fc8997136bca97694972fc92970344bf41626f
Wesley Seago <wseago2@fullsail.edu>
2013-08-18 19:20:50 -0400

    Setup week 3

---
+++ ProjectWeek3/Project3/Project3/AddEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddEventViewController.h
+//  Project3
+//
+//  Created by Wesley Seago on 8/18/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEventViewController : UIViewController
+
+@end
---
+++ ProjectWeek3/Project3/Project3/AddEventViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddEventViewController.m
+//  Project3
+//
+//  Created by Wesley Seago on 8/18/13.
+//  Copyright (c) 2013 Wesley Seago. All rights reserved.
+//
+
+#import "AddEventViewController.h"
+
+@interface AddEventViewController ()
+
+@end
+
+@implementation AddEventViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
